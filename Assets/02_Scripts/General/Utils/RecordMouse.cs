8cf3634ce13e0a66db1c0028cea2d21b4eaaa638
skypoweras <skypoweras@gmain.com>
2025-02-05 18:26:24 +0900

    녹화 커서 추가

---
+++ Assets/02_Scripts/General/Utils/RecordMouse.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordMouse : MonoBehaviour
+{
+    public RectTransform cursorImage;
+
+    private void Awake()
+    {
+        cursorImage.gameObject.SetActive(false);
+    }
+
+#if UNITY_EDITOR // Unity Editor에서만 실행
+    void Start()
+    {
+        cursorImage.gameObject.SetActive(true);
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        cursorImage.position = mousePosition;
+    }
+#endif
+}
