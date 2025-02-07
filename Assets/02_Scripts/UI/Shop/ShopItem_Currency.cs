646452c7ad05c08e8ea43c80bce069341a8e29f6
skypoweras <skypoweras@gmain.com>
2025-02-07 16:02:17 +0900

    상점팝업에 재화 카테고리 추가

---
+++ Assets/02_Scripts/UI/Shop/ShopItem_Currency.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem_Currency : MonoBehaviour
+{
+    [SerializeField] private int _getCurrencyCount;     // 획득할 재화 수
+    [SerializeField] private Button _buyButton;         // 구매버튼
+    [SerializeField] private TextMeshProUGUI _costText; // 비용 텍스트
+    [SerializeField] private GameObject _dimd;          // 딤드
+
+
+}
